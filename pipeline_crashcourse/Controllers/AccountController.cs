3a7e055bda884d35980e343fdb35573bceec7f38
DiosLaz <67984004+DiosLaz@users.noreply.github.com>
2025-01-19 16:00:23 +0100

    setup

---
+++ pipeline_crashcourse/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pipeline_crashcourse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("/Login")]
+        public IActionResult Logins()
+        {
+            return Ok("Works fines login updated");
+        }
+
+
+        [HttpGet("/Register")]
+        public IActionResult Registers()
+        {
+            return Ok("Works fine, register updated");
+        }
+    }
+}
